c199e8714b95ce8c8f0a0729b69526763da4e9fd
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-03 10:25:13 +0200

    creates signup

---
+++ api/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel.js");
+
+// Middleware to check if the user is authenticated
+exports.isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({ message: "No token, authorization denied" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Find the full user by ID from the decoded token
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Attach the full user object to req.user
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { test } from "../controllers/userController.js";
+import { signup, test } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.post('/signup', signup)
     
 
 export default router;
