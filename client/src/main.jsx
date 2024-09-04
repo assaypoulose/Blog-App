ece25781c43d8507a848ac1b0c5e9f056cd15116
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-04 15:09:13 +0200

    created Redux Persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 import './index.css';
-import { store } from './redux/store';
+import { store, persitor } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-    </Provider>,
-)
+  <PersistGate persistor={persitor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
+);
