1c2f8baeb002fa67b3f0b3c3545901223f78feff
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-05 18:01:13 +0200

    created show users UI component

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiUser, HiUserGroup } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
@@ -51,6 +51,14 @@ export default function DashSidebar() {
                     <Link to='/dashboard?tab=posts'>
                     <Sidebar.Item active={tab === 'posts'} icon={HiDocumentText} as='div'>
                         Posts
+                    </Sidebar.Item>
+                </Link>
+                )}
+
+                { currentUser.isAdmin && (
+                    <Link to='/dashboard?tab=users'>
+                    <Sidebar.Item active={tab === 'users'} icon={HiUserGroup} as='div'>
+                        Users
                     </Sidebar.Item>
                 </Link>
                 )}
