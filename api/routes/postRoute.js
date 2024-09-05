4ccce48b9e753194dda6f986011241cb144a0f38
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-05 15:32:56 +0200

    created updatepost api route

--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import { create, getposts, deletepost } from '../controllers/postController.js';
+import { create, getposts, deletepost, updatepost } from '../controllers/postController.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create)
-router.get('/getposts', getposts)
-router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.post('/create', verifyToken, create);
+router.get('/getposts', getposts);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost);
 
 
 export default router;
