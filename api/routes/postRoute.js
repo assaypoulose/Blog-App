747f87957545e6f51ec39b2162852932f05ee8cf
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-05 13:03:32 +0200

    created getposts api route

--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import { create } from '../controllers/postController.js';
+import { create, getposts } from '../controllers/postController.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/getposts', getposts)
 
 
 export default router;
