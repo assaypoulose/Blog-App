f7bb48492a9f773a562d5ec26163aaab1d4611f1
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-05 17:08:00 +0200

    created getusers api route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, signin, google, updateUser, deleteUser, signout } from "../controllers/userController.js";
+import { signup, signin, google, updateUser, deleteUser, signout, getUsers } from "../controllers/userController.js";
 import { verifyToken } from '../middleware/auth.js'
 
 const router = express.Router();
@@ -9,7 +9,8 @@ router.post('/signin', signin);
 router.post('/google', google); //signin using google account
 router.put('/update/:userId', verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser);
-router.post('/signout', signout)
+router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
     
 
 export default router;
