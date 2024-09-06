576b5b410b1c7f747f6d184ada3a86413c28bc30
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-06 11:25:52 +0200

    created like and edit api route

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import { createComment, getPostComments } from '../controllers/commentController.js';
+import { createComment, getPostComments, likeComment, editComment } from '../controllers/commentController.js';
 
 
 const router = express.Router();
@@ -8,6 +8,8 @@ const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 
 
 
