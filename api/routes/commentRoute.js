af9085426f109790e76bea07caf37d7d989dc7c3
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-09-06 11:27:28 +0200

    created deletecomments and getcomments api routes

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import { createComment, getPostComments, likeComment, editComment } from '../controllers/commentController.js';
+import { createComment, getPostComments, likeComment, editComment, deleteComment, getcomments } from '../controllers/commentController.js';
 
 
 const router = express.Router();
@@ -10,6 +10,8 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 
 
